f459e8f88b9ea3e0df8cb49d36e0588fa662d717
Andrew Chan <andrewlfchan@google.com>
2025-08-28 03:11:36 -0400

    Change CORS URL

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 // Create a new file called config.js in your public folder
 const API_BASE_URL = window.location.hostname === 'localhost' 
     ? 'http://localhost:3000'
-    : 'https://branddna.googleplex.com';
+    : 'https://branddna.web.app';
 
 // Export for use in other files
 window.API_BASE_URL = API_BASE_URL;
