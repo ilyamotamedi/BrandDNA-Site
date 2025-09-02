55ec956358f08ea60ce44c32412c49a4d31071e6
Andrew Chan <andrewlfchan@google.com>
2025-09-02 13:08:36 -0400

    Update config to internal site

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 // Create a new file called config.js in your public folder
 const API_BASE_URL = window.location.hostname === 'localhost' 
     ? 'http://localhost:3000'
-    : 'https://branddna.web.app';
+    : 'https://test--branddna.us-central1.hosted.app';
 
 // Export for use in other files
 window.API_BASE_URL = API_BASE_URL;
--- src/configs/cors.configs.js
+++ src/configs/cors.configs.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 const corsConfig = cors({
   origin: process.env.NODE_ENV === 'production'
-    ? ['https://branddna.web.app']
+    ? ['https://test--branddna.us-central1.hosted.app']
     : 'http://localhost:3000',
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
